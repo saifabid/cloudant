55f4a8201f81362e776041a538f560364da5db01
abidsaif <saif.abid@mail.utoronto.ca>
2016-12-25 21:36:09 -0500

    Cloudant API returns code + message in response so instead of interfaces + custom structs return a regular error

--- cloudant.go
+++ cloudant.go
@@ -2,6 +2,7 @@ package cloudant
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -15,10 +16,6 @@ const (
 	LessThan    = "$lt"
 	Equal       = "$eq"
 	Asc         = "asc"
-)
-
-const (
-	pkgErrorCode = 0
 )
 
 // DB defines the parameters needed to make API calls against a specific database
@@ -38,60 +35,6 @@ type Query struct {
 	Skip     int
 }
 
-// Err defines the interface met by by API Errors or any other errors while using this package
-type Err interface {
-	Error() string
-	Message() map[string]string
-	StatusCode() int
-}
-
-// PkgError defines any errors which are not returned from cloudant
-type PkgError struct {
-	err error
-}
-
-// Error returns the error reason as a string
-func (p PkgError) Error() string {
-	return p.err.Error()
-}
-
-// Message returns the error as a map
-func (p PkgError) Message() map[string]string {
-	return map[string]string{
-		"error": p.err.Error(),
-	}
-}
-
-// StatusCode returns a default status of 0 signifying its a package error and not an HTTP error
-func (p PkgError) StatusCode() int {
-	return pkgErrorCode
-}
-
-// APIError defines the params for a non 2xx response we get back from cloudant
-type APIError struct {
-	message        map[string]string
-	httpStatusCode int
-}
-
-// Error returns the cloudant response error reason
-func (a APIError) Error() string {
-	if a.message["reason"] == "" {
-		return "unknown error, check Message() method"
-	}
-
-	return a.message["reason"]
-}
-
-// Message returns the cloudant response body unmarhsaled into a map when the response is not 2xx
-func (a APIError) Message() map[string]string {
-	return a.message
-}
-
-// StatusCode returns the http status code returned by cloudant when the response is not 2xx
-func (a APIError) StatusCode() int {
-	return a.httpStatusCode
-}
-
 // Setup inits all the params needed to make further requests to the cloudant API
 func Setup(username, password, database, host string) *DB {
 	return &DB{
@@ -107,29 +50,22 @@ func (db *DB) newRequest() *gorequest.SuperAgent {
 }
 
 // Insert inserts a doccument and returns the rev of the doccument created
-func (db *DB) Insert(doc interface{}) (string, Err) {
+func (db *DB) Insert(doc interface{}) (string, error) {
 	url := fmt.Sprintf("%s/%s", db.Host, db.Database)
 	req := db.newRequest()
 	resp, body, errs := req.Post(url).SendStruct(doc).EndBytes()
 	if errs != nil {
-		return "", PkgError{
-			err: errs[0],
-		}
+		return "", errs[0]
 	}
 
 	if resp.StatusCode/100 != 2 {
 		var v map[string]string
 		err := json.Unmarshal(body, &v)
 		if err != nil {
-			return "", PkgError{
-				err: err,
-			}
+			return "", errs[0]
 		}
 
-		return "", APIError{
-			httpStatusCode: resp.StatusCode,
-			message:        v,
-		}
+		return "", errors.New(string(body))
 	}
 
 	type respJSON struct {
@@ -139,67 +75,51 @@ func (db *DB) Insert(doc interface{}) (string, Err) {
 	var respBody respJSON
 	err := json.Unmarshal(body, &respBody)
 	if err != nil {
-		return "", PkgError{
-			err: errs[0],
-		}
+		return "", err
 	}
 
 	return respBody.Rev, nil
 }
 
 // GetByID gets a single doccument by it's _id
-func (db *DB) GetByID(id string, params map[string]string) ([]byte, Err) {
+func (db *DB) GetByID(id string, params map[string]string) ([]byte, error) {
 	url := fmt.Sprintf("%s/%s/%s?%s", db.Host, db.Database, id, mapToQueryString(params))
 	req := db.newRequest()
 	resp, body, errs := req.Get(url).EndBytes()
 	if errs != nil {
-		return nil, PkgError{
-			err: errs[0],
-		}
+		return nil, errs[0]
 	}
 
 	if resp.StatusCode%100 != 2 {
 		var v map[string]string
 		err := json.Unmarshal(body, &v)
 		if err != nil {
-			return nil, PkgError{
-				err: err,
-			}
+			return nil, err
 		}
 
-		return nil, APIError{
-			httpStatusCode: resp.StatusCode,
-			message:        v,
-		}
+		return nil, errors.New(string(body))
 	}
 
 	return body, nil
 }
 
 // Update will update a single doccument with the new doccument and returns the rev of the doccument updated
-func (db *DB) Update(id string, doc interface{}) (string, Err) {
+func (db *DB) Update(id string, doc interface{}) (string, error) {
 	url := fmt.Sprintf("%s/%s/%s", db.Host, db.Database, id)
 	req := db.newRequest()
 	resp, body, errs := req.Put(url).SendStruct(doc).EndBytes()
 	if errs != nil {
-		return "", PkgError{
-			err: errs[0],
-		}
+		return "", errs[0]
 	}
 
 	if resp.StatusCode/100 != 2 {
 		var v map[string]string
 		err := json.Unmarshal(body, &v)
 		if err != nil {
-			return "", PkgError{
-				err: err,
-			}
+			return "", err
 		}
 
-		return "", APIError{
-			httpStatusCode: resp.StatusCode,
-			message:        v,
-		}
+		return "", errors.New(string(body))
 	}
 
 	type respJSON struct {
@@ -209,38 +129,29 @@ func (db *DB) Update(id string, doc interface{}) (string, Err) {
 	var respBody respJSON
 	err := json.Unmarshal(body, &respBody)
 	if err != nil {
-		return "", PkgError{
-			err: errs[0],
-		}
+		return "", errs[0]
 	}
 
 	return respBody.Rev, nil
 }
 
 // Delete will delete a doccument
-func (db *DB) Delete(id, rev string) Err {
+func (db *DB) Delete(id, rev string) error {
 	url := fmt.Sprintf("%s/%s/%s?rev=%s", db.Host, db.Database, id, rev)
 	req := db.newRequest()
 	resp, body, errs := req.Delete(url).EndBytes()
 	if errs != nil {
-		return PkgError{
-			err: errs[0],
-		}
+		return errs[0]
 	}
 
 	if resp.StatusCode/100 != 2 {
 		var v map[string]string
 		err := json.Unmarshal(body, &v)
 		if err != nil {
-			return PkgError{
-				err: err,
-			}
+			return err
 		}
 
-		return APIError{
-			httpStatusCode: resp.StatusCode,
-			message:        v,
-		}
+		return errors.New(string(body))
 	}
 
 	return nil
